afccb839c531880b6f05307027ec9af390c02148
Bogdan <quick07102000@gmail.com>
2025-09-13 13:48:07 +0200

    123

--- ConsoleApplication21111.cpp
+++ ConsoleApplication21111.cpp
@@ -1,24 +1,46 @@
-// ConsoleApplication21111.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstring>
+#include <Windows.h>
 
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    cout << "Hello Underworld!\n";
-    cout << "Uraaa";
-}
+struct Date {
+    int day;
+    int month;
+    int year;
+};
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+struct Fio {
+    char* lname;
+    char* name;
+    char* mname;
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+class Student {
+    Fio fio;
+    Date BirthDate;
+    char* phone;
+    char* city;
+    char* country;
+    char* university;
+    char* UCity;
+    char* UCountry;
+    char* group;
+
+};
+
+void intput() {
+
+    char buffer[100];
+    cout << "¬вед≥ть пр≥звище: ";
+    cin.getline(buffer, 100);
+
+    }
+
+
+    int main() {
+    
+        SetConsoleOutputCP(65001);
+        SetConsoleCP(65001);
+    
+    }
